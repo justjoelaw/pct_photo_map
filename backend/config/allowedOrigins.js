ec307209c0496a4a083296991217c2b107f5b31c
Joe Lawson <justjoe_1994@hotmail.co.uk>
2023-06-30 16:54:20 +0100

    remove trailing backslash from cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:3000', 'https://trail-journal.azurewebsites.net/'];
+const allowedOrigins = ['http://localhost:3000', 'https://trail-journal.azurewebsites.net'];
 
 export default allowedOrigins;
